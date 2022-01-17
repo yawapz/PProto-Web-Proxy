78b9f0c9a7af24828584fbdd30683dd68f1d0417
Alexander Shilov <aleksandr.schilow2012@gmail.com>
2022-01-17 11:08:45 +0300

    Перенос проверки совместимости протоколов в прокси

--- proxy.js
+++ proxy.js
@@ -1,5 +1,6 @@
 const net = require('net');
 const WebSocket = require( "ws");
+const {v4: uuid} = require('uuid');
 
 const ORIGIN_PORT = process.env['ORIGIN_PORT'] || 8000;
 const ORIGIN_HOST = process.env['ORIGIN_HOST'] || 'localhost';
@@ -12,10 +13,6 @@ const PROTOCOL_SIGNATURE = [0xfe, 0xa6, 0xb9, 0x58, 0xda, 0xfb, 0x4f, 0x5c, 0xb6
 const decoder = new TextDecoder();
 const encoder = new TextEncoder();
 
-function trimMessage(message, maxLength = 400) {
-    return message.length > maxLength ? message.substring(0, maxLength - 3) + '...' : message;
-}
-
 function main() {
     const wsServer = new WebSocket.Server({port: PROXY_PORT, host: PROXY_HOST});
 
@@ -24,6 +21,11 @@ function main() {
         console.log(`[${connectionId}] Client connected`);
         wsClient.pause();
 
+        function disconnect() {
+            tcpSocket.destroy();
+            wsClient.close();
+        }
+
         const tcpSocket = net.connect({
             port: ORIGIN_PORT,
             host: ORIGIN_HOST,
@@ -31,48 +33,94 @@ function main() {
         }, async () => {
             console.log(`[${connectionId}] Server connected`);
 
-            tcpSocket.write(new Uint8Array(PROTOCOL_SIGNATURE));
-            const signature = await readSocketAsync(tcpSocket, 16);
-            const isSignatureMatch = isArrayEquals(signature, PROTOCOL_SIGNATURE);
-
-            if (!isSignatureMatch) {
-                console.log(`[${connectionId}] Signature is invalid, disconnecting`);
-                tcpSocket.destroy();
-                wsClient.close();
+            try {
+                await validateSignature(connectionId, tcpSocket);
+                await checkCompatibility(connectionId, tcpSocket);
+            } catch (e) {
+                console.error(`[${connectionId}] Connection error, disconnecting. ${e}`);
+                disconnect();
                 return;
-            } else {
-                console.log(`[${connectionId}] Signature is valid`);
             }
 
             wsClient.on('message', (messageBuffer) => {
-                const message = decoder.decode(messageBuffer);
-                console.log(`[${connectionId}] Message from client`, trimMessage(message));
-                sendMessage(tcpSocket, message);
+                try {
+                    const message = decoder.decode(messageBuffer);
+                    console.log(`[${connectionId}] Message from client`, trimMessage(message));
+                    sendMessage(tcpSocket, message);
+                } catch (e) {
+                    console.error(`[${connectionId}] Failed to process client message, disconnecting. ${e}`);
+                    disconnect();
+                }
             });
             wsClient.resume();
 
             while (tcpSocket.readable) {
-                const message = await receiveMessage(tcpSocket);
-                console.log(`[${connectionId}] Message from server`, trimMessage(message));
-                wsClient.send(message);
+                try {
+                    const message = await receiveMessage(tcpSocket);
+                    console.log(`[${connectionId}] Message from server`, trimMessage(message));
+                    wsClient.send(message);
+                } catch (e) {
+                    console.error(`[${connectionId}] Failed to process server message, disconnecting. ${e}`);
+                    disconnect();
+                }
             }
         });
 
         wsClient.on('close', () => {
             console.log(`[${connectionId}] Client disconnected`);
-            tcpSocket.destroy();
+            disconnect();
         });
 
         tcpSocket.on('close', () => {
             console.log(`[${connectionId}] Server disconnected`);
-            wsClient.close();
+            disconnect();
         });
 
         tcpSocket.on('error', (e) => {
-            console.error(`[${connectionId}] Server error. ${e}`);
-            wsClient.close();
+            console.error(`[${connectionId}] Server error, disconnecting. ${e}`);
+            disconnect();
         });
     });
+}
+
+function trimMessage(message, maxLength = 400) {
+    return message.length > maxLength ? message.substring(0, maxLength - 3) + '...' : message;
+}
+
+async function validateSignature(connectionId, tcpSocket) {
+    tcpSocket.write(new Uint8Array(PROTOCOL_SIGNATURE));
+    const signature = await readSocketAsync(tcpSocket, 16);
+    const isSignatureMatch = isArrayEquals(signature, PROTOCOL_SIGNATURE);
+
+    if (!isSignatureMatch) {
+        throw new Error('Signature mismatch');
+    }
+
+    console.log(`[${connectionId}] Signature is valid`);
+}
+
+async function checkCompatibility(connectionId, tcpSocket) {
+    const request = {
+        id: uuid(),
+        command: "173cbbeb-1d81-4e01-bf3c-5d06f9c878c3",
+        flags: 2164291649,
+        content: null,
+        tags: null
+    };
+    let response;
+
+    try {
+        await sendMessage(tcpSocket, JSON.stringify(request));
+        response = JSON.parse(await receiveMessage(tcpSocket)) || {};
+    } catch (e) {
+        throw new Error(`Failed to check compatibility. ${e}`);
+    }
+
+    if (response.command !== request.command) {
+        throw new Error(`Protocol is not compatible, server response: ${response.command}`);
+    }
+
+    console.log(`[${connectionId}] Protocol is compatible`);
 }
 
 async function receiveMessage(socket) {
